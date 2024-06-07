52bcda9f34371dbaaee68d32e7e6820ab53f66e8
vbuarque <viniciusbuarque17@gmail.com>
2024-06-07 17:19:51 -0300

    feat: estruturando o componente de header e estilizando

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,13 @@
 import styles from './styles.module.css'
+import IgniteLogo from '../../assets/ignite-logo.svg';
 
 export function Header() {
     return(
       <header className={styles.header}>
-        <strong>Ignite Feed</strong>
+        <div className={styles.logoContainer}>
+          <img src={IgniteLogo} alt="Logotipo do Ignite Feed" />
+          <strong>Ignite Feed</strong>
+        </div>
       </header>  
     );
 } 
