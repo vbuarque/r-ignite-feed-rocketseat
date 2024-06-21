8e40d06b5b65e746743eb474faf6dbc04dd6a0bd
vbuarque <viniciusbuarque17@gmail.com>
2024-06-20 21:56:32 -0300

    style: finalizando a estilização do componente Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,7 +3,7 @@ import styles from './Post.module.css';
 export function Post() {
     return (
         <article className={styles.post}>
-            <header>
+            <header className={styles.authorHeader}>
                 <div className={styles.author}>
                     <img className={styles.avatar} src="https://github.com/vbuarque.png" alt="" />
 
@@ -17,27 +17,25 @@ export function Post() {
             </header>
 
             <div className={styles.content}>
-                <p>
-                    Fala galeraa 👋
-                </p>
-
-                <p>
-                    Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
-                </p>
-
-                <p>
-                    👉
-                    <a href="">
-                        jane.design/doctorcare
-                    </a>
-                </p>
-
-                <p>
-                    <a href="">
-                        #novoprojeto #nlw #rocketseat
-                    </a>
+                <p>Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p>👉{' '}<a href="">jane.design/doctorcare</a></p>
+                <p className={styles.hashtags}>
+                    <a href="">#novoprojeto</a>
+                    <a href="">#nlw</a>
+                    <a href="">#rocketseat</a>
                 </p>
             </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+                <textarea
+                    placeholder="Escreva um comentário..."
+                />
+                <footer>
+                    <button type="submit">Publicar</button>
+                </footer>
+            </form>
         </article>
     )
 }
